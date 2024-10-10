ee6c49290ce87ff1979bb1405ff6ef11aea2d526
云深 <yunshen4869@gmmail.com>
2024-10-10 18:50:33 +0800

    添加导航动画

--- app/src/main/java/com/yunshen/yunapp/navigation/NavHost.kt
+++ app/src/main/java/com/yunshen/yunapp/navigation/NavHost.kt
@@ -1,5 +1,7 @@
 package com.yunshen.yunapp.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -19,7 +21,17 @@ fun Navigation(
     viewmodel: UIViewModel,
     storeManager: StoreManager
 ) {
-    NavHost(navController = navHostController, startDestination = Destinations.INDEX.name) {
+    NavHost(navController = navHostController,
+        startDestination = Destinations.INDEX.name,
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(700)
+            )
+        },
+        exitTransition = {
+            slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left, animationSpec = tween(700))
+        }) {
         composable(route = Destinations.INDEX.name) {
             IndexScreen(modifier = modifier, viewModel = viewmodel)
         }
